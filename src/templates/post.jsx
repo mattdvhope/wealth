4c6809d2c1b10872c9ac18f10383aa7d5dc4b98a
mattdvhope <mattagape@me.com>
2018-07-25 08:58:37 +0700

    Commented out Disqus

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -91,7 +91,9 @@ export default class PostTemplate extends React.Component {
             config={config}
             expanded={expanded}
           />
+        {/* 
           <Disqus postNode={postNode} expanded={expanded} />
+           */}
         </div>
 
         <PostSuggestions postNode={postNode} />
